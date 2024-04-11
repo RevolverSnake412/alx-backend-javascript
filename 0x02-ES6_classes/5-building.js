fb5f4dedd5dc1423ba1510293f231c5cf91ae260
RevolverSnake412 <oussag@hotmail.fr>
2024-04-11 21:19:55 +0000

    TASK 6

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  /*
+   * @param {Number} sqft.
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError 'Sqft must be a number';
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
