3132633ae320c270ff572478fcfd846c4c4727ac
RevolverSnake412 <oussag@hotmail.fr>
2024-04-11 19:29:00 +0000

    TASK 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const room = new ClassRoom(19);
+  const room1 = new ClassRoom(20);
+  const room2 = new ClassRoom(34);
+
+  return [room, room1, room2];
+}
+
+export default initializeRooms;
