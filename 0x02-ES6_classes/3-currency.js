bf69c2508b89de0143e9fec1a22b44cc6be36210
RevolverSnake412 <oussag@hotmail.fr>
2024-04-11 20:09:13 +0000

    TASK 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,43 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  /*
+   * @param {String} code
+   */
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  /*
+   * @param {String} name
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  /*
+   * @method - return the attributes in the following format name (code).
+  */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+export default Currency;
