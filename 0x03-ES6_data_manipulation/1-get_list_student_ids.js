c57a4b5f4179959014ba0b55cd62d85d9a04b9e6
rs412-wsl_ubuntu_18.04LTS <oussag@hotmail.fr>
2024-04-15 18:49:03 +0100

    TASK 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(object) {
+    let array = [];
+    if (object instanceof Array) {
+      array = object.map((item) => item.id);
+    }
+    return array;
+  }
+export default getListStudentIds;
